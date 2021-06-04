b1565907f6c3122898178b47e6b0aad3c3b04183
tiziano <tiziano_marinaro@outlook.it>
2021-06-04 09:13:50 +0200

    modified list of registers

--- arch_bir.py
+++ arch_bir.py
@@ -19,21 +19,23 @@ class ArchBIR(Arch):
         super().__init__(Endness.LE)
 
     register_list = [
-        Register(name="R0", size=8, vex_offset=0),
-        Register(name="R1", size=8, vex_offset=8),
-        Register(name="R2", size=8, vex_offset=16),
-        Register(name="R3", size=8, vex_offset=24),
-        Register(name="R4", size=8, vex_offset=32),
-        Register(name="R5", size=8, vex_offset=40),
-        Register(name="R6", size=8, vex_offset=48),
-        Register(name="R7", size=8, vex_offset=56),
-        Register(name="R8", size=8, vex_offset=64),
-        Register(name="R9", size=8, vex_offset=72),
-        Register(name="R10", size=8, vex_offset=80),
-        Register(name="SP_EL0", size=8, vex_offset=88),
-        Register(name="ptr", size=8, vex_offset=96),
-        Register(name="inout", size=1, vex_offset=104),
-        Register(name="ip_at_syscall", size=8, vex_offset=112),
+        Register(name="ip", size=8, vex_offset=0),
+        Register(name="R0", size=8, vex_offset=8),
+        Register(name="R1", size=8, vex_offset=16),
+        Register(name="R2", size=8, vex_offset=24),
+        Register(name="R3", size=8, vex_offset=32),
+        Register(name="R4", size=8, vex_offset=40),
+        Register(name="R5", size=8, vex_offset=48),
+        Register(name="R6", size=8, vex_offset=56),
+        Register(name="R7", size=8, vex_offset=64),
+        Register(name="R8", size=8, vex_offset=72),
+        Register(name="R9", size=8, vex_offset=80),
+        Register(name="R10", size=8, vex_offset=88),
+        Register(name="SP_EL0", size=8, vex_offset=96),
+        Register(name="ProcState_Z", size=8, vex_offset=104),
+        Register(name="ptr", size=8, vex_offset=112),
+        Register(name="inout", size=1, vex_offset=120),
+        Register(name="ip_at_syscall", size=8, vex_offset=128),
     ]
 
 
