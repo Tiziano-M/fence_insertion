e3b39c8ee442af78cd632ce3dec1f8a0b980302f
tiziano <tiziano_marinaro@outlook.it>
2021-06-04 09:19:29 +0200

    changed the lifter to list of irsb

--- lift_bir.py
+++ lift_bir.py
@@ -11,6 +11,10 @@ import logging
 
 l = logging.getLogger(__name__)
 
+import angr
+import pyvex
+from angr.engines import SimEngine, SimSuccessors#, SimEngineVEX, SimEngineProcedure, SimEngineUnicorn
+from angr.engines.vex import HeavyVEXMixin, TrackActionsMixin, SimInspectMixin, HeavyResilienceMixin, SuperFastpathMixin
 
 
 class LifterBIR(Lifter):
@@ -23,22 +27,17 @@ class LifterBIR(Lifter):
     
 
     def lift(self, disassemble=False, dump_irsb=False):
-        blocks = self.parse(self.data)
-        irsb_c = IRSBCustomizer(self.irsb)
+        #blocks = self.parse(self.data)
+        #irsb_c = IRSBCustomizer(self.irsb)
 
         try:
             bir_Instruction = BIR_Instruction(arch=archinfo.arch_from_id('bir'), addr=0)
-            for block in blocks:
-                irsb_c.imark(block.label, bir_Instruction.bytewidth, 0)
-                for statements in block.statements:
-                    bir_Instruction.map_statements(statements, irsb_c)
-                bir_Instruction.map_statements(block.last_statement, irsb_c)
-                #irsb_c.imark(bir_Instruction.addr, bir_Instruction.bytewidth, 0)
 
-            irsb_c.irsb.jumpkind = JumpKind.NoDecode
-            dst = irsb_c.irsb.addr + irsb_c.irsb.size
-            dst_ty = vex_int_class(irsb_c.irsb.arch.bits).type
-            irsb_c.irsb.next = irsb_c.mkconst(dst, dst_ty)
+            irsb_c = IRSBCustomizer(self.irsb)
+            irsb_c.imark(block.label, bir_Instruction.bytewidth, 0)
+            for statements in block.statements:
+                bir_Instruction.map_statements(statements, irsb_c)
+            bir_Instruction.map_statements(block.last_statement, irsb_c)
         except:
             raise LiftingException('Could not decode any instructions')
         if dump_irsb:
@@ -59,7 +58,16 @@ if __name__ == '__main__':
 
     bir_input = open("examples/bir_program.txt", "r")
     lifter = LifterBIR(arch=archinfo.arch_from_id('bir'), addr=2)
-    lifter._lift(data=bir_input)
-    lifter.irsb.pp()
+    bir_program = ParserBIR(bir_input)
+    blocks = bir_program.parse()
+    irsb_list = list()
+    for block in blocks:
+        lifter._lift(data=block)
+        lifter.irsb.pp()
+        irsb_list.append(lifter.irsb)
+
+
+    test = HeavyVEXMixin(project=None)
+    print(test)
 
 	
