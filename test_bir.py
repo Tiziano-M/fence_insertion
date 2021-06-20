965455d02707445e3c5729738cb39635e445221e
tiziano <tiziano_marinaro@outlook.it>
2021-06-21 00:33:10 +0200

    rewritten in a better form

--- test_bir.py
+++ test_bir.py
@@ -1,15 +1,33 @@
-import archinfo
-from arch_bir import ArchBIR
-from parse_bir import ParserBIR
-from lift_bir import LifterBIR
+import angr
+import angr_platforms.bir
 
 
-bir_input = open("examples/bir_program.bir", "r")
-lifter = LifterBIR(arch=archinfo.arch_from_id('bir'), addr=2)
-bir_program = ParserBIR(bir_input)
-blocks = bir_program.parse()
-irsb_list = list()
-for block in blocks:
-    lifter._lift(data=block)
-    lifter.irsb.pp()
-    irsb_list.append(lifter.irsb)
+def test():
+    proj = angr.Project("examples/test.bir", main_opts={'backend': 'bir'})
+
+    state = proj.factory.entry_state()
+    simgr = proj.factory.simulation_manager(state)
+    #simgr._stashes
+    #simgr._errored
+    #simgr.deadended
+    simgr.explore()
+
+
+def test_unicorn():
+    from angr.engines.unicorn import SimEngineUnicorn
+    proj = angr.Project("examples/test.bir", main_opts={'backend': 'bir'}, engine=SimEngineUnicorn)
+
+    #add different code for unicron
+    #state = proj.factory.entry_state()
+    #simgr = proj.factory.simulation_manager(state)
+    #simgr.explore()
+
+
+
+def main():
+    test()
+    #test_unicorn()
+
+
+if __name__ == '__main__':
+    main()
