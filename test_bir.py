e2b025c0e249e2960f7e74408f6764cbfe1243ae
tiziano <tiziano_marinaro@outlook.it>
2021-06-23 19:46:35 +0200

    added test with the initialised registers

--- test_bir.py
+++ test_bir.py
@@ -3,14 +3,35 @@ import angr_platforms.bir
 
 
 def test():
-    proj = angr.Project("examples/test.bir", main_opts={'backend': 'bir'})
+    proj = angr.Project("examples/bir_program.bir", main_opts={'backend': 'bir'})
 
     state = proj.factory.entry_state()
     simgr = proj.factory.simulation_manager(state)
     #simgr._stashes
-    #simgr._errored
-    #simgr.deadended
     simgr.explore()
+
+
+def test2():
+    proj = angr.Project("examples/test2.bir", main_opts={'backend': 'bir', 'base_addr': 3489667176})
+
+    state = proj.factory.entry_state(addr=3489667176)
+    simgr = proj.factory.simulation_manager(state)
+    simgr.explore()
+
+
+def test3():
+    import claripy
+    proj = angr.Project("examples/test3.bir", main_opts={'backend': 'bir'})
+
+    state = proj.factory.entry_state()
+    state.regs.R3 = claripy.BVS("R3", 64)
+    state.regs.R8 = claripy.BVS("R8", 64)
+    state.regs.ProcState_Z = claripy.BVS("ProcState_Z", 8)
+    simgr = proj.factory.simulation_manager(state)
+    simgr.explore()
+    print(simgr.deadended)
+    print(simgr.deadended[0].regs.R9)
+    print(simgr.deadended[1].regs.R10)
 
 
 def test_unicorn():
@@ -26,7 +47,6 @@ def test_unicorn():
 
 def main():
     test()
-    #test_unicorn()
 
 
 if __name__ == '__main__':
