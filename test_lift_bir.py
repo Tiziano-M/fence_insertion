f365be944de59da1c6cac4bf51f5abfebb5a9fdc
tiziano <tiziano_marinaro@outlook.it>
2021-06-25 17:20:39 +0200

    fixed some tests

--- test_lift_bir.py
+++ test_lift_bir.py
@@ -1,23 +1,33 @@
+#######################################################
+######## other tests in pyvex/tests/test.py
+#######################################################
+
+import archinfo
+import pyvex
+from arch_bir import ArchBIR
+from parse_bir import ParserBIR
+from lift_bir import LifterBIR
+
+
+bir_input = open("examples/test.bir", "rb")
+bir_input = bir_input.read()
 
 
 def test1():
-    import archinfo
-    from arch_bir import ArchBIR
-    from parse_bir import ParserBIR
-    from lift_bir import LifterBIR
-
-    bir_input = open("examples/test.bir", "rb")
-    bir_input = bir_input.read()
-    lifter = LifterBIR(arch=archinfo.arch_from_id('bir'), addr=2)
-    #bir_program = ParserBIR(bir_input)
-    #blocks = bir_program.parse()
-    #irsb_list = list()
-    #for block in blocks:
+    lifter = LifterBIR(arch=archinfo.arch_from_id('bir'), addr=0)
     lifter._lift(data=bir_input)
-    #lifter.irsb.pp()
-    #irsb_list.append(lifter.irsb)
+    lifter = LifterBIR(arch=archinfo.arch_from_id('bir'), addr=4)
+    lifter._lift(data=bir_input)
+    lifter = LifterBIR(arch=archinfo.arch_from_id('bir'), addr=8)
+    lifter._lift(data=bir_input)
 
 
+def test_two_irsb():
+    irsb1 = pyvex.IRSB(data=bir_input, mem_addr=0, arch=archinfo.arch_from_id('bir'))
+    irsb2 = pyvex.IRSB(data=bir_input, mem_addr=4, arch=archinfo.arch_from_id('bir'))
+
+
+# broken
 def test2():
     import angr
     import __init__
@@ -29,8 +39,8 @@ def test2():
 
 
 def main():
-    #test1()
-    test2()
+    test1()
+    #test_two_irsb()
 
 
 if __name__ == '__main__':
