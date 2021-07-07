effb711058f051ba36b2c88963d31f5d854a07f8
tiziano <tiziano_marinaro@outlook.it>
2021-07-07 16:57:13 +0200

    added get_block to manage system calls

--- lift_bir.py
+++ lift_bir.py
@@ -14,48 +14,77 @@ l = logging.getLogger(__name__)
 
 
 
+
 class LifterBIR(Lifter):
     lifter.VEX_IRSB_MAX_SIZE = 10000
+    bir_Instruction = BIR_Instruction(arch=archinfo.arch_from_id('bir'), addr=0)
 
-    cache_data = None
+    cache_blocks = None
+    previous_block = None
+    count_previous_block = 0 # number of executed statements of the previous block
 
-    def get_data(data):
-        if LifterBIR.cache_data is None:
-            LifterBIR.cache_data = data
-        return LifterBIR.cache_data
-
-
-    def parse(self):
-        data = "".join(chr(i) for i in LifterBIR.get_data(self.data))
+    def parse(data):
+        data = "".join(chr(i) for i in data)
         #print(data)
-        #print(self.addr)
         parser = ParserBIR(data)
         blocks = parser.parse()
         return blocks
+    
+    def get_blocks(self):
+        if LifterBIR.cache_blocks is None:
+            LifterBIR.cache_blocks = LifterBIR.parse(self.data)
+        return LifterBIR.cache_blocks
 
+    def get_block(self, blocks):
+        print(self.addr)
+        print(LifterBIR.count_previous_block)
+        for b in blocks:
+            if b.label == self.addr:
+                block = b
+                break
+            else:
+                if LifterBIR.previous_block is None:
+                    block = None
+                else:
+                    block = LifterBIR.previous_block
+                    block.statements = block.statements[LifterBIR.count_previous_block:]
+                    LifterBIR.count_previous_block = 0
+                    LifterBIR.previous_block = None
+                    break
+        return block
+        
 
     def lift(self, dump_irsb=True):
 
         try:
-            blocks = self.parse()
-            bir_Instruction = BIR_Instruction(arch=archinfo.arch_from_id('bir'), addr=0)
-
-            try:
-                block = next(b for b in blocks if b.label == self.addr)
-            except:
-                block=None
+            blocks = self.get_blocks()
+            block = self.get_block(blocks)
 
             if block is None:
                 irsb_c = IRSBCustomizer(self.irsb)
                 irsb_c.imark(self.addr, 1, 0)
                 irsb_c.irsb.jumpkind = JumpKind.Exit
             else:
+                LifterBIR.count_previous_block = 0
                 irsb_c = IRSBCustomizer(self.irsb)
                 irsb_c.imark(block.label, 1, 0)
                 for statements in block.statements:
-                    bir_Instruction.map_statements(statements, irsb_c)
-                bir_Instruction.map_statements(block.last_statement, irsb_c)
+                    if self.irsb.jumpkind != JumpKind.Syscall:
+                        LifterBIR.count_previous_block += 1
+                    else:
+                        LifterBIR.previous_block = block
+                        dst = 0x1
+                        dst_ty = vex_int_class(irsb_c.irsb.arch.bits).type
+                        irsb_c.irsb.next = irsb_c.mkconst(dst, dst_ty)
+                        irsb_c.irsb.jumpkind = JumpKind.Boring
+                        if dump_irsb:
+                            self.irsb.pp()
+                        return self.irsb
+                    LifterBIR.bir_Instruction.map_statements(statements, irsb_c)
+                LifterBIR.bir_Instruction.map_statements(block.last_statement, irsb_c)
+                                          
 
+                # 2 way to manage the exit
                 #if not any(block.label == int(str(irsb_c.irsb.next), 16) for block in blocks):
                 #    self.irsb.jumpkind = JumpKind.Exit
         except:
@@ -63,7 +92,6 @@ class LifterBIR(Lifter):
             raise LiftingException('Could not decode any instructions')
         if dump_irsb:
             self.irsb.pp()
-            #print(self.irsb.size)
         return self.irsb
         
 
