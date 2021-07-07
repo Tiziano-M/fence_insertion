17d7d560fbb95d7f1e66fdd159f5ec9e34e40b5a
tiziano <tiziano_marinaro@outlook.it>
2021-07-07 16:51:20 +0200

    added SystemCall for Observation

--- simos_bir.py
+++ simos_bir.py
@@ -1,15 +1,40 @@
 from angr.simos import SimUserland, register_simos
-from angr.calling_conventions import SimStackArg, SimRegArg, SimCC, register_syscall_cc, register_default_cc, SimCC
+from angr.calling_conventions import SimStackArg, SimRegArg, SimCC, register_syscall_cc, register_default_cc
+from angr import SimProcedure
+from angr.procedures import SIM_PROCEDURES as P, SIM_LIBRARIES as L
+from angr.procedures.definitions import SimSyscallLibrary
 
 from arch_bir import ArchBIR
 
 
+
+class Observation(SimProcedure):
+
+    num_args = 1
+    NUM_ARGS = 1
+
+    def run(self, obs):
+        print("\nObservation:", obs, "\n")
+        #self.state.obs_list.append(obs)
+        #self.jump(0x3) 
+
+
+
+P['bir'] = {}
+P['bir']['observation'] = Observation
+
+syscall_lib = SimSyscallLibrary()
+syscall_lib.set_library_names('bir')
+syscall_lib.add_all_from_dict(P['bir'])
+syscall_lib.add_number_mapping_from_dict('BIR', {0 : 'observation'})
+
+
 class SimCCBIR(SimCC):
-    ARG_REGS = [ ]
+    ARG_REGS = ['obs']
     FP_ARG_REGS = [ ]
     STACKARG_SP_DIFF = 0
-    RETURN_ADDR = SimStackArg(0, 8)
-    RETURN_VAL = SimRegArg('ip', 8)
+    RETURN_ADDR = SimRegArg('ip', 8)
+    RETURN_VAL = SimRegArg('obs', 8)
     ARCH = ArchBIR
 
 
@@ -17,8 +42,8 @@ class SimBIR(SimUserland):
     # Syscalls are for lamers
     SYSCALL_TABLE = {}
 
-    def __init__(self, *args, **kwargs):
-        super(SimBIR, self).__init__(*args, name="BIR", **kwargs)
+    def __init__(self, project, **kwargs):
+        super(SimBIR, self).__init__(project, syscall_library=L['bir'], name="BIR", **kwargs)
 
     def state_blank(self, data_region_size=0x8000, **kwargs): # pylint:disable=arguments-differ
         state = super(SimBIR, self).state_blank(**kwargs)  # pylint:disable=invalid-name
@@ -30,8 +55,9 @@ class SimBIR(SimUserland):
 
 
 class SimBIRSyscall(SimCC):
-    ARG_REGS = [ ]
-    # RETURN_VAL = ""
+    ARG_REGS = ['obs']
+    RETURN_ADDR = SimRegArg('ip', 8)
+    RETURN_VAL = SimRegArg('obs', 8)
     ARCH = ArchBIR
 
     @staticmethod
@@ -41,7 +67,8 @@ class SimBIRSyscall(SimCC):
 
     @staticmethod
     def syscall_num(state):
-        return state.regs.inout
+        return state.regs.ip_at_syscall
+
 
 register_simos('BIR', SimBIR)
 register_syscall_cc('BIR', 'default', SimBIRSyscall)
