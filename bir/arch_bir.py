66a025f4cd00c8c4f1ddcb73d39429bce73273d2
tiziano <tiziano_marinaro@outlook.it>
2021-08-20 23:14:32 +0200

    add the dynamic setting of the register list

--- bir/arch_bir.py
+++ bir/arch_bir.py
@@ -1,5 +1,58 @@
 from archinfo.arch import Arch, Register, Endness
 from archinfo.arch import register_arch
+import json
+
+
+def regs_extraction_from_json(birprog, regs):
+    try:
+        if isinstance(birprog, list):
+            for dic in birprog:
+                if isinstance(dic, dict):
+                    for key,value in dic.items():
+                        if key == "var":
+                            reg = dic.get(key)
+                            if reg["name"] != "MEM":
+                                regs.append(reg)
+                        else:
+                            regs_extraction_from_json(dic[key], regs)
+        elif isinstance(birprog, dict):
+            for key,value in birprog.items():
+                if key == "var":
+                    reg = birprog.get(key)
+                    if reg["name"] != "MEM":
+                        regs.append(reg)
+                else:
+                    regs_extraction_from_json(birprog[key], regs)
+    except:
+        raise Exception("Error of bir program in json format")
+    return regs
+
+def get_register_list(birprog):
+    with open(birprog, "r") as json_file:
+        birprogjson = json.load(json_file)
+    # gets all BVAR in the json BIR program
+    regs = regs_extraction_from_json(birprogjson, [])
+    # removes dupilcates using hashed tuples
+    regs = [dict(t) for t in {tuple(sorted(d.items())) for d in regs}]
+    # reorders the list
+    regs = sorted(regs, key=lambda k: k['name'])
+    
+    vex_offset = 24
+    for reg in regs:
+        if reg["type"] == "imm64":
+            sz = 8
+        elif reg["type"] == "imm32":
+            sz = 4
+        elif reg["type"] == "imm16":
+            sz = 2
+        elif reg["type"] == "imm8":
+            sz = 1
+        elif reg["type"] == "imm1":
+            sz = 1
+        vex_offset = vex_offset + 8
+        ArchBIR.register_list.append(Register(name=reg["name"], size=sz, vex_offset=vex_offset))
+
+    return regs
 
 
 class ArchBIR(Arch):
@@ -20,50 +73,9 @@ class ArchBIR(Arch):
 
     register_list = [
         Register(name="ip", size=8, alias_names=('pc'), vex_offset=0),
-        Register(name="R0", size=8, vex_offset=8),
-        Register(name="R1", size=8, vex_offset=16),
-        Register(name="R2", size=8, vex_offset=24),
-        Register(name="R3", size=8, vex_offset=32),
-        Register(name="R4", size=8, vex_offset=40),
-        Register(name="R5", size=8, vex_offset=48),
-        Register(name="R6", size=8, vex_offset=56),
-        Register(name="R7", size=8, vex_offset=64),
-        Register(name="R8", size=8, vex_offset=72),
-        Register(name="R9", size=8, vex_offset=80),
-        Register(name="R10", size=8, vex_offset=88),
-        Register(name="R11", size=8, vex_offset=96),
-        Register(name="R12", size=8, vex_offset=104),
-        Register(name="R13", size=8, vex_offset=112),
-        Register(name="R14", size=8, vex_offset=120),
-        Register(name="R15", size=8, vex_offset=128),
-        Register(name="R16", size=8, vex_offset=136),
-        Register(name="R17", size=8, vex_offset=144),
-        Register(name="R18", size=8, vex_offset=152),
-        Register(name="R19", size=8, vex_offset=160),
-        Register(name="R20", size=8, vex_offset=168),
-        Register(name="R21", size=8, vex_offset=176),
-        Register(name="R22", size=8, vex_offset=184),
-        Register(name="R23", size=8, vex_offset=192),
-        Register(name="R24", size=8, vex_offset=200),
-        Register(name="R25", size=8, vex_offset=208),
-        Register(name="R26", size=8, vex_offset=216),
-        Register(name="R27", size=8, vex_offset=224),
-        Register(name="R28", size=8, vex_offset=232),
-        Register(name="R29", size=8, vex_offset=240),
-        Register(name="R30", size=8, vex_offset=248),
-        Register(name="R31", size=8, vex_offset=256),
-        Register(name="SP_EL0", size=8, vex_offset=264),
-        Register(name="SP_EL1", size=8, vex_offset=272),
-        Register(name="SP_EL2", size=8, vex_offset=280),
-        Register(name="SP_EL3", size=8, vex_offset=288),
-        Register(name="ProcState_C", size=1, vex_offset=289),
-        Register(name="ProcState_E", size=1, vex_offset=290),
-        Register(name="ProcState_N", size=1, vex_offset=291),
-        Register(name="ProcState_V", size=1, vex_offset=292),
-        Register(name="ProcState_Z", size=1, vex_offset=293),
-        Register(name="obs", size=8, vex_offset=301),
-        Register(name="ip_at_syscall", size=8, vex_offset=309),
-        Register(name="syscall_num", size=8, vex_offset=317)
+        Register(name="obs", size=8, vex_offset=8),
+        Register(name="ip_at_syscall", size=8, vex_offset=16),
+        Register(name="syscall_num", size=8, vex_offset=24)
     ]
 
 
--- symbolic_execution.py
+++ symbolic_execution.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 import angr
 import bir
@@ -13,48 +14,24 @@ args = parser.parse_args()
 
 
 
-def init_regs(state):
-    state.regs.R0 = claripy.BVS("R0", 64)
-    state.regs.R1 = claripy.BVS("R1", 64)
-    state.regs.R2 = claripy.BVS("R2", 64)
-    state.regs.R3 = claripy.BVS("R3", 64)
-    state.regs.R4 = claripy.BVS("R4", 64)
-    state.regs.R5 = claripy.BVS("R5", 64)
-    state.regs.R6 = claripy.BVS("R6", 64)
-    state.regs.R7 = claripy.BVS("R7", 64)
-    state.regs.R8 = claripy.BVS("R8", 64)
-    state.regs.R9 = claripy.BVS("R9", 64)
-    state.regs.R10 = claripy.BVS("R10", 64)
-    state.regs.R11 = claripy.BVS("R11", 64)
-    state.regs.R12 = claripy.BVS("R12", 64)
-    state.regs.R13 = claripy.BVS("R13", 64)
-    state.regs.R14 = claripy.BVS("R14", 64)
-    state.regs.R15 = claripy.BVS("R15", 64)
-    state.regs.R16 = claripy.BVS("R16", 64)
-    state.regs.R17 = claripy.BVS("R17", 64)
-    state.regs.R18 = claripy.BVS("R18", 64)
-    state.regs.R19 = claripy.BVS("R19", 64)
-    state.regs.R20 = claripy.BVS("R20", 64)
-    state.regs.R21 = claripy.BVS("R21", 64)
-    state.regs.R22 = claripy.BVS("R22", 64)
-    state.regs.R23 = claripy.BVS("R23", 64)
-    state.regs.R24 = claripy.BVS("R24", 64)
-    state.regs.R25 = claripy.BVS("R25", 64)
-    state.regs.R26 = claripy.BVS("R26", 64)
-    state.regs.R27 = claripy.BVS("R27", 64)
-    state.regs.R28 = claripy.BVS("R28", 64)
-    state.regs.R29 = claripy.BVS("R29", 64)
-    state.regs.R30 = claripy.BVS("R30", 64)
-    state.regs.R31 = claripy.BVS("R31", 64)
-    state.regs.SP_EL0 = claripy.BVS("SP_EL0", 64)
-    state.regs.SP_EL1 = claripy.BVS("SP_EL1", 64)
-    state.regs.SP_EL2 = claripy.BVS("SP_EL2", 64)
-    state.regs.SP_EL3 = claripy.BVS("SP_EL3", 64)
-    state.regs.ProcState_C = claripy.BVS("ProcState_C", 8)
-    state.regs.ProcState_E = claripy.BVS("ProcState_E", 8)
-    state.regs.ProcState_N = claripy.BVS("ProcState_N", 8)
-    state.regs.ProcState_V = claripy.BVS("ProcState_V", 8)
-    state.regs.ProcState_Z = claripy.BVS("ProcState_Z", 8)
+def set_registers(birprog):
+    regs = bir.arch_bir.get_register_list(birprog)
+    return regs
+
+
+def init_regs(state, regs):
+    for reg in regs:
+        if reg["type"] == "imm64":
+            sz = 64
+        elif reg["type"] == "imm32":
+            sz = 32
+        elif reg["type"] == "imm16":
+            sz = 16
+        elif reg["type"] == "imm8":
+            sz = 8
+        elif reg["type"] == "imm1":
+            sz = 8
+        setattr(state.regs, reg["name"], claripy.BVS(reg["name"], sz))
         
 
 def add_state_options(state):
@@ -65,28 +42,42 @@ def add_state_options(state):
 def print_results(final_states):
     print("\n\n")
     print(f"RESULT: {len(final_states)} final states")
+
+    output = []
+    dict_state = {}
     for state in final_states:
         print("="*80)
         print("STATE:", state)
+        dict_state["addr"] = str(state)
         # is a listing of the basic block addresses executed by the state.
         list_addrs = state.history.bbl_addrs.hardcopy
         # converts addresses from decimal to hex
         list_addrs = list(map(lambda value: hex(value), list_addrs))
-        list_guards = state.history.jump_guards.hardcopy
-        list_obs = state.observations.get_list_obs()
+        list_guards = [str(guard) for guard in state.history.jump_guards.hardcopy]
+        list_obs = [str(obs) for obs in state.observations.get_list_obs()]
         print("\t- Path:\t\t", list_addrs)
         print("\t- Guards:\t", list_guards)
         print("\t- Observations:\t", list_obs)
         print("="*80)
+        dict_state["path"] = list_addrs
+        dict_state["guards"] = list_guards
+        dict_state["observations"] = list_obs
+        output.append(dict_state)
+    # in the end, prints the json output
+    json_object = json.dumps(output, indent=4)
+    print(json_object)
 
 
 def main():
+    # extracts the registers from the input program and sets them in the register list of the architecture
+    regs = set_registers(args.program)
+
     # initializes the angr project
     proj = angr.Project(args.program, main_opts={'base_addr': args.base_addr})
 
     # sets the initial state and registers
     state = proj.factory.entry_state(addr=args.base_addr)
-    init_regs(state)
+    init_regs(state, regs)
     add_state_options(state)
 
     # executes the symbolic execution and prints the results
